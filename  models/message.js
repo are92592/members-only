02be98e121eec781265f0c48112e20e745491c46
aerhartic <aerhartic@gmail.com>
2021-05-11 15:37:17 -0400

    controllers and models updated

---
+++  models/message.js
@@ -0,0 +1,13 @@
+
+
+/* 
+
+mongoose.schema Message = {
+    date_and_time: new Date() object
+    time_since_post: current date - original date, round to whatevt is the closest to 0
+    user: Schema.user (user id) (user.name and user.username)
+    title: String
+    script: String
+}
+
+*/
---
+++  models/user.js
@@ -0,0 +1,18 @@
+
+
+/* 
+
+mongoose schema User = {
+    first_name: String
+    last_name: String
+    username: String
+    password: String
+    membership_status: boolean
+    messages: Schema.message (with the user's id)
+}
+
+virtual schema = name
+
+virtual schema = account label (username + password)
+
+*/
